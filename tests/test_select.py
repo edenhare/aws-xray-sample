2f91c42b777fce3f18574c837478dbebe21b7301
pi@pi1 <pi@pi1>
2021-01-07 20:21:32 -0600

    create sample pytest

--- tests/test_select.py
+++ tests/test_select.py
@@ -1,9 +1,14 @@
 from functions.poc_select_pair import app
 import datetime
+from aws_xray_sdk.core import xray_recorder
 
 def test_select():
 
-    data = app.lambda_handler(event, "")
+    # Start a segment
+    segment = xray_recorder.begin_segment('test-select-pair')
+    data = app.lambda_handler({}, "")
+    # End a segnent
+    xray_recorder.end_segment()
 
     assert "statusCode" in data
 
